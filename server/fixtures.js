73958354dd7042ef3b46e564632301809a371bb9
alessandroparisi <alessandro.parisi@mail.mcgill.ca>
2015-06-11 00:22:45 -0700

    buy filter functionality

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,12 +1,14 @@
-function insertPhone(OS, company, model, capacities, colors) {
+
+
+function insertPhone(OS, company, model, storage, color) {
 	Phones.insert({
 		os: OS, // at first, only iOS or Android. Maybe blackberry and others later.
 		company: company, 
 		model: model,
-		capacities: capacities,
-		colors: colors
+		storage: storage,
+		color: color
 	})
-};
+}
 
 if(Phones.find().count() === 0) {
 
@@ -18,9 +20,30 @@ if(Phones.find().count() === 0) {
 	insertPhone('iOS', 'Apple', 'iPhone 5', ['16', '32', '64'], ['Black', 'White']);
 	insertPhone('iOS', 'Apple', 'iPhone 4s', ['8', '16', '32', '64'], ['Black', 'White']);
 	insertPhone('iOS', 'Apple', 'iPhone 4', ['8', '16', '32'], ['Black', 'White']);
-
-	// Android phones
-	// TODO
 }
 
+function insertAd(company, model, storage, color) {
+	Ads.insert({
+		company: company, 
+		model: model,
+		storage: storage,
+		color: color
+	})
+}
 
+if(Ads.find().count() === 0) {
+
+	// iPhones
+	insertAd('Apple', 'iPhone 6', '16', 'Gold');
+	insertAd('Apple', 'iPhone 5', '32', 'Gold');
+	insertAd('Apple', 'iPhone 5s', '16', 'Silver');
+	insertAd('Apple', 'iPhone 5c', '16', 'White');
+	insertAd('Apple', 'iPhone 4', '16', 'Black');
+	insertAd('Apple', 'iPhone 4s', '8', 'White');
+	insertAd('Apple', 'iPhone 4', '32', 'White');
+	insertAd('Apple', 'iPhone 6', '128', 'Space Gray');
+	insertAd('Apple', 'iPhone 5', '16', 'Gold');
+	insertAd('Apple', 'iPhone 6 Plus', '128', 'Silver');
+	insertAd('Apple', 'iPhone 4', '32', 'Black');
+	insertAd('Apple', 'iPhone 5', '16', 'White');
+}
